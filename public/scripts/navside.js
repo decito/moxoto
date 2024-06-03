68ae0c0a75a1f0f55c286e15f7b52fe206e38b1b
Decio Neto <deciom.neto10@gmail.com>
2024-06-02 22:56:32 -0300

    feat: add mobile navbar

---
+++ public/scripts/navside.js
@@ -0,0 +1,9 @@
+function openNavside() {
+	document.getElementById("nav-side").style.width = "250px";
+	document.getElementById("mask-container").style.display = "block";
+}
+
+function closeNavside() {
+	document.getElementById("nav-side").style.width = "0";
+	document.getElementById("mask-container").style.display = "none";
+}
